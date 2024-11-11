3f118bdbf1aa6bd1eb6d6b11694fe8cd2a4ebfca
zhadok2002saidova <zhadyratalgatovna1006@gmail.com>
2024-11-11 13:00:11 +0500

    Add files via upload
    exam4 done

---
+++ script.js
@@ -0,0 +1,39 @@
+var months = prompt("1 ден 12 ге дейін сан енгіз");
+
+if (months == 3) {
+  alert("3 ай  - көктем мезгілі");
+} else if (months == 4) {
+  alert("4 ай - көктем мезгілі ");
+} else if (months == 5) {
+  alert("5 ай - көктем мезгілі ");
+}
+
+var months1 = prompt("1 ден 12 ге дейін сан енгіз");
+
+if (months1 == 6) {
+  alert("6 ай  - жаз мезгілі");
+} else if (months1 == 7) {
+  alert("7 ай - жаз мезгілі ");
+} else if (months1 == 8) {
+  alert("8 ай - жаз мезгілі ");
+}
+
+var months2 = prompt("1 ден 12 ге дейін сан енгіз");
+
+if (months2 == 9) {
+  alert("9 ай - күз мезгілі");
+} else if (months2 == 10) {
+  alert("10 ай - күз мезгілі ");
+} else if (months2 == 11) {
+  alert("11 ай - күз мезгілі ");
+}
+
+var months3 = prompt("1 ден 12 ге дейін сан енгіз");
+
+if (months3 == 12) {
+  alert("12 ай - қыс мезгілі");
+} else if (months3 == 1) {
+  alert("1 ай - қыс мезгілі ");
+} else if (months3 == 2) {
+  alert("2 ай - қыс мезгілі ");
+}
